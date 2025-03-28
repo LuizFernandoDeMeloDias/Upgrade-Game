70acd0739db0ed60235d2a4e8a7f58cdbb4fa401
Luiz Dias <valitio999@gmail.com>
2025-03-28 11:55:28 -0300

    Atualizando sistema de inventário

--- scripts/Inventario.gd
+++ scripts/Inventario.gd
@@ -4,66 +4,36 @@ export(NodePath) var grid_container_path
 var itens = []
 var maxslots = 16
 
-var selectedSlot = 0
+var indexSelectedSlot = 0
 
-
+func atualizar_slot(index):
+	get_node("GridContainer").get_child(index).dados_slot[index] = itens[index]
+	pass
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_scroll_down") and selectedSlot != 0:
-		selectedSlot -= 1
-		get_node("GridContainer").get_child(selectedSlot).modulate = 'ff4a4a'
-		get_node("GridContainer").get_child(selectedSlot + 1).modulate = 'ffffff'
-	if Input.is_action_just_pressed("ui_scroll_up") and selectedSlot != 15:
-		selectedSlot += 1
-		get_node("GridContainer").get_child(selectedSlot).modulate = 'ff4a4a'
-		get_node("GridContainer").get_child(selectedSlot - 1).modulate = 'ffffff'
+	if Input.is_action_just_pressed("ui_scroll_down") and indexSelectedSlot != 0:
+		indexSelectedSlot -= 1
+		get_node("GridContainer").get_child(indexSelectedSlot).modulate = 'ff4a4a'
+		get_node("GridContainer").get_child(indexSelectedSlot + 1).modulate = 'ffffff'
+	if Input.is_action_just_pressed("ui_scroll_up") and indexSelectedSlot != 15:
+		indexSelectedSlot += 1
+		get_node("GridContainer").get_child(indexSelectedSlot).modulate = 'ff4a4a'
+		get_node("GridContainer").get_child(indexSelectedSlot - 1).modulate = 'ffffff'
 	
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		if get_node("GridContainer").get_child(selectedSlot).dados_slot["nome"] == 'Regador':
-			for node in get_tree().get_nodes_in_group("areaPlant"):
-				node.regar = true
-		else:
-			for node in get_tree().get_nodes_in_group("areaPlant"):
-				node.regar = false
-
-
-
-
 func _ready():
-	
-	criar_slots(maxslots)
-	get_node("GridContainer").get_child(selectedSlot).modulate = 'ff4a4a'
+	atualizar_itens()
+	get_node("GridContainer").get_child(indexSelectedSlot).modulate = 'ff4a4a'
 
-func criar_slots(maxslots):
-	var slots = preload("res://cenas/SlotItem.tscn")
-	for i in range(maxslots):
-		var slot_instance = slots.instance()
-		slot_instance.name = 'emptySlot' + str(i)
-		itens.append(
-			{
-				'nome': "null",
-				'icone': null,
-				'description': 'null',
-				'amount': 0,
-				'estocavel': false
-			}
-		)
-		get_node("GridContainer").add_child(slot_instance)
+
+func atualizar_itens():
+	pass
 
 func add_item(amount, description, nome, icon, estocavel):
 	for i in range(len(itens)):
 		if itens[i]["nome"] == nome and itens[i]["estocavel"]:
-			var slot = get_node("GridContainer").get_child(i)
 			itens[i]["amount"] += amount
-			slot.atualizar_amount(itens[i]["amount"])
 			return
-
-	# Impede duplicação de itens não estocáveis
-	for i in range(len(itens)):
-		if itens[i]["nome"] == nome and not itens[i]["estocavel"]:
-			return
-
+			
 	for i in range(len(itens)):
 		if itens[i]["nome"] == 'null':
 			itens[i] = {
@@ -72,8 +42,7 @@ func add_item(amount, description, nome, icon, estocavel):
 				"icone": icon,
 				"description": description,
 				"estocavel": estocavel
-			}
-			var slot = get_node("GridContainer").get_child(i)
-			slot.atualizar_slot(nome, icon, amount, description)
-			return
+		}
+			atualizar_slot(i)
+
 
--- texture/SlotItem.gd
+++ texture/SlotItem.gd
@@ -1,11 +1,18 @@
 extends TextureButton
 
 var dados_slot = {
-	"nome": '',
+	"nome": null,
 	"icon": null,
 	"totalAmount": 0,
 	"description": '',
 }
+func _ready():
+	get_tree().call_group('inventario', 'atualizar_itens', 
+	dados_slot['nome'], 
+	dados_slot['icon'],
+	dados_slot['totalAmount'], 
+	dados_slot['description']
+	)
 
 func atualizar_slot(nome, icon, amount, description):
 	dados_slot["nome"] = nome
@@ -34,42 +41,20 @@ func get_drag_data(position):
 	var preview = TextureRect.new()
 	preview.texture = dados_slot["icon"]
 	set_drag_preview(preview)
-
-	var data_to_send = dados_slot.duplicate()
-	data_to_send["origem"] = self 
-	return data_to_send
+	return dados_slot
 
 func can_drop_data(position, data):
 	return data.has("nome")
 
 func drop_data(position, data):
-	if "origem" in data and data["origem"] != self:
-		
-		atualizar_slot(data["nome"], data["icon"], data["totalAmount"], data["description"])
-		
-		var origem_index = data["origem"].get_index()
-		var destino_index = get_index()
-		
-		var inventory_manager = get_tree().get_nodes_in_group("inventario")[0]
-		inventory_manager.itens[destino_index] = inventory_manager.itens[origem_index]
-		inventory_manager.itens[origem_index] = {
-			'nome': "null",
-			'icone': null,
-			'description': 'null',
-			'amount': 0,
-			'estocavel': false
-		}
+	atualizar_slot(data["nome"], data["icon"], data["totalAmount"], data["description"])
 
-		data["origem"].limpar_slot()
-
-func limpar_slot():
-	dados_slot = {
-		"nome": '',
-		"icon": null,
-		"totalAmount": 0,
-		"description": '',
+func set_empty_slot():
+	dados_slot  = {
+	"nome": '',
+	"icon": null,
+	"totalAmount": 0,
+	"description": '',
 	}
-	get_node("itemTexture").texture = null
-	get_node("slottexture/amount").text = ""
-	get_node("slottexture/background/description").text = ""
+
 
